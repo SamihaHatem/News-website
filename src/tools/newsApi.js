f5dc616b2eef75c587bac366f1185f9ecc75fc56
SamihaHatem <98450607+SamihaHatem@users.noreply.github.com>
2022-03-15 16:09:53 +0200

    version 2

--- src/tools/newsApi.js
+++ src/tools/newsApi.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const newsApi = (callbackFun) => {
-    const newsURL = 'https://newsapi.org/v2/top-headlines?country=eg&apiKey='
+    const newsURL = 'https://newsapi.org/v2/top-headlines?country=eg&apiKey=66a34cc266c64b6eaa477c8b15c876ae'
 
     request({ url: newsURL, json: true }, (error, response) => {
         if (error)
